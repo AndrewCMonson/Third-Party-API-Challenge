28a4830724b18eaae13b4386f31f61b3fb0f64f9
Andrew Monson <139920681+AndrewCMonson@users.noreply.github.com>
2023-10-19 21:27:48 -0400

    get and set storage functions complete

--- Develop/script.js
+++ Develop/script.js
@@ -9,9 +9,48 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-    const saveInput = () => {
 
+  const getStorage = () => JSON.parse(localStorage.getItem('Stored Inputs'));
+  
+  const updateStorage = (arr) => {
+    const storedArr = getStorage();
+    console.log(storedArr);
+    if(!storedArr){
+      localStorage.setItem('Stored Inputs', JSON.stringify(arr))
+    }if(storedArr){
+      const newStoredArr = [...storedArr, arr];
+      localStorage.setItem('Stored Inputs', JSON.stringify(newStoredArr))
     }
+
+  }
+
+
+  $('.time-block').on('click', '.saveBtn', e => {
+      e.preventDefault()
+      console.log('description value', $(e.target).siblings('.description').val());
+      console.log($(e.target).siblings('.hour').text())
+
+      // grabs the hour value from the DOM, finds the first number in the string and converts it to an integer
+      const hour = $(e.target).siblings('.hour').text();
+      const trueHour = hour.match(/[0-9]+/);
+      const trueHourValue = parseInt(trueHour);
+      // grabs the user input from the DOM and stores it in a variable
+      const userInputValue = $(e.target).siblings('.description').val();
+      // array to store inputs from the user
+      const storedInputs = [];
+      // inputs from the user placed into an object
+      const savedInput = {
+        hour: trueHourValue,
+        textInput: userInputValue
+      }
+      storedInputs.push(savedInput);
+
+      // localStorage.setItem('Stored Inputs', JSON.stringify(storedInputs));
+      updateStorage(storedInputs);
+
+    })
+    
+    
     
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,7 +59,9 @@ $(function () {
   // current hour in 24-hour time?
   //
     const determineTime = () => {
-
+      const hour = $(e.target).siblings('.hour').text();
+      const trueHour = hour.match(/[0-9]+/);
+      const trueHourValue = parseInt(trueHour);
     }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -34,9 +75,7 @@ $(function () {
     const displayTime = () => {
       const now = dayjs().format('MM/DD/YYYY HH:MM:ss');
       const timeDisplay = $('#currentDay');
-      timeDisplay.text(now);
-
-      
+      timeDisplay.text(now); 
     }
 
     setInterval(displayTime, 1000);
