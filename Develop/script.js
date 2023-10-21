ace1c8d7b6f67ffd7f7791dbb541038994f48c06
Andrew Monson <139920681+AndrewCMonson@users.noreply.github.com>
2023-10-20 22:15:27 -0400

    reworked the event listener to have an independent function that can be called within it

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,6 @@ $(function () {
   // function used to grab a stored array from local storage
   // can be used for any stored array and it will parse
   const getStoredArr = (localStorageArr) => JSON.parse(localStorage.getItem(localStorageArr));
-  
   /*
     - This function takes an array that has only one object as argument
     - If there is not an array stored in local storage, it creates one based on the passed array
@@ -32,76 +31,75 @@ $(function () {
       localStorage.setItem(storedInputsStr, JSON.stringify(storedArr))
     }
   }
-
   /*
-    - This event listener uses a click on the '.saveBtn" class of buttons to save the value of the '.description' class and the hour it belongs to, into an object. 
-    - After the object is created, it is pushed into an array
+    - This function will be used by a click event listener to capture the input of the targeted scheduler time-block.
+    - It will create an object with the input and it's associated timevalue and push it into an array
     - Finally, the created array of objects is passed as an argument into the updateDailyVals function which updates LS.
   */
-  $('.time-block').on('click', '.saveBtn', e => {
-      e.preventDefault()
-      // grabs the hour value from the DOM, finds the first number in the string and converts it to an integer
-      const hour = $(e.target).siblings('.hour').text();
+  const captureInput = (e) => {
+    e.preventDefault()
+    // grabs the hour value from the DOM, finds the first number in the string and converts it to an integer
+    const hour = $(e.target).siblings('.hour').text();
+    const trueHour = hour.match(/[0-9]+/);
+    const trueHourValue = parseInt(trueHour);
+    // grabs the user input from the DOM and stores it in a variable
+    const userInputValue = $(e.target).siblings('.description').val();
+    // array to store inputs from the user
+    const storedInputs = [];
+    // inputs from the user placed into an object
+    const savedInput = {
+      hour: trueHourValue,
+      textInput: userInputValue
+    }
+    storedInputs.push(savedInput);
+    updateDailyVals(storedInputs);
+  }
+    
+  /*
+    - determineTime is run on page load and takes no arguments
+    - it initializes dayjs and formats it into a 24 hour clock
+    - next, the function gathers all of the timeblocks from the DOM via jQuery
+    - next, it gathers the id from each time block and converts it to an integer
+    - next, it compares the integer to the current hour of dayjs
+    - if the current hour is greater than the hour on the time block, it adds the '.past' class turning the block grey.
+    - if the current hour is less than the hour on the time block, it add the '.future' class and turns the block green
+    - if the hours match, the block is turned red
+  */
+  const determineTime = () => {
+    // const hour = $(e.target).siblings('.hour').text();
+    // const trueHour = hour.match(/[0-9]+/);
+    // const trueHourValue = parseInt(trueHour);
+    const now = dayjs().format('H');
+    console.log(now)
+    const timeBlocks = $('.time-block');
+    console.log(timeBlocks);
+
+    for(let i = 0; i < timeBlocks.length; i++){
+      const currentTimeBlock = timeBlocks[i];
+      const hour = currentTimeBlock.id;
       const trueHour = hour.match(/[0-9]+/);
       const trueHourValue = parseInt(trueHour);
-      // grabs the user input from the DOM and stores it in a variable
-      const userInputValue = $(e.target).siblings('.description').val();
-      // array to store inputs from the user
-      const storedInputs = [];
-      // inputs from the user placed into an object
-      const savedInput = {
-        hour: trueHourValue,
-        textInput: userInputValue
-      }
-      storedInputs.push(savedInput);
-      updateDailyVals(storedInputs);
-    })
-    
-    /*
-      - determineTime is run on page load and takes no arguments
-      - it initializes dayjs and formats it into a 24 hour clock
-      - next, the function gathers all of the timeblocks from the DOM via jQuery
-      - next, it gathers the id from each time block and converts it to an integer
-      - next, it compares the integer to the current hour of dayjs
-      - if the current hour is greater than the hour on the time block, it adds the '.past' class turning the block grey.
-      - if the current hour is less than the hour on the time block, it add the '.future' class and turns the block green
-      - if the hours match, the block is turned red
-    */
-    const determineTime = () => {
-      // const hour = $(e.target).siblings('.hour').text();
-      // const trueHour = hour.match(/[0-9]+/);
-      // const trueHourValue = parseInt(trueHour);
-      const now = dayjs().format('H');
-      console.log(now)
-      const timeBlocks = $('.time-block');
-      console.log(timeBlocks);
 
-      for(let i = 0; i < timeBlocks.length; i++){
-        const currentTimeBlock = timeBlocks[i];
-        const hour = currentTimeBlock.id;
-        const trueHour = hour.match(/[0-9]+/);
-        const trueHourValue = parseInt(trueHour);
-
-        if(trueHourValue < now){
-          $(currentTimeBlock).addClass('past');
-        }else if(trueHourValue > now){
-          $(currentTimeBlock).addClass('future')
-        }else{
-          $(currentTimeBlock).addClass('present')
-        }
+      if(trueHourValue < now){
+        $(currentTimeBlock).addClass('past');
+      }else if(trueHourValue > now){
+        $(currentTimeBlock).addClass('future')
+      }else{
+        $(currentTimeBlock).addClass('present')
       }
     }
+  }
 
     determineTime();
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-    const getInput = () => {
+    const updateDom = () => {
 
     }
 
-    
+
     // This displays the current time on the page and updates every second
     const displayTime = () => {
       const now = dayjs().format('MM/DD/YYYY HH:MM:ss');
@@ -110,4 +108,5 @@ $(function () {
     }
 
     setInterval(displayTime, 1000);
+    $('.time-block').on('click', '.saveBtn', captureInput)
 });
